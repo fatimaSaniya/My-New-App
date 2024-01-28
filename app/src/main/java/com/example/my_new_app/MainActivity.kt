a38f4fa08e84be39866a4a0da1f3f57e28556b21
saniya <mirzasaniya716@gmail.com>
2024-01-28 20:48:21 +0530

    Adding image

--- app/src/main/java/com/example/my_new_app/MainActivity.kt
+++ app/src/main/java/com/example/my_new_app/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.my_new_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
@@ -46,6 +48,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -122,15 +125,15 @@ fun ScreenOne() {
                 }
             }
         }
-//        Image(
-//            painter = painterResource(id = R.drawable.doctor),
-//            contentDescription = null,
-//            alignment = Alignment.Center,
-//            modifier = Modifier
-//                .padding(top = 150.dp)
-//                .width(400.dp)
-//                .size(270.dp)
-//        )
+        Image(
+            painter = painterResource(id = R.drawable.doctor1),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .padding(top = 150.dp)
+                .width(400.dp)
+                .size(270.dp)
+        )
         Row {
             var text by remember { mutableStateOf("") }
 
